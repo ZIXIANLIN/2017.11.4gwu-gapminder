f866e8b4a47bc65b7ca432b0edd7d8229a738ef1
ZIXIANLIN <541404345@qq.com>
2017-11-04 10:14:08 -0400

    initial

---
+++ Scrip.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+download.file("http://fmichonneau.github.io/2017-11-03-gwu/gapminder.csv",
+              "data/gapminder.csv")
+gapminder <- read.csv("data/gapminder.csv")
+
+#calucate the average expectancy by continent 
+#and save it in a variable call life_exp_continent
+life_exp_continent <- gapminder %>% 
+  group_by(continent) %>% 
+  summarise(
+    mean_life_exp = mean(lifeExp)
+  )
